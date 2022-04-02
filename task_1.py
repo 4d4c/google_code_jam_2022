eb8f8ade975df8eae3f837b9d88c339945f73186
4d4c <>
2022-04-02 11:54:37 +0100

    Punched Cards - Pass

--- task_1.py
+++ task_1.py
@@ -1,5 +1,29 @@
-def solution():
-    return 0
+from ossaudiodev import control_labels
+
+
+def solution(rows, columns):
+    card = ""
+
+    for row_count in range(0, int(rows) * 2 + 1):
+        for col_count in range(0, int(columns) * 2 + 1):
+            if row_count < 2 and col_count < 2:
+                card += "."
+                continue
+
+            if col_count % 2 == 0:
+                if row_count % 2 == 0:
+                    card += "+"
+                else:
+                    card += "|"
+            else:
+                if row_count % 2 == 0:
+                    card += "-"
+                else:
+                    card += "."
+
+        card += "\n"
+
+    return card
 
 def main():
     number_of_test_cases = int(input())
@@ -8,7 +32,7 @@ def main():
         line_read = input()
         result = solution(*line_read.split(" "))
 
-        print("Case #{}: {}".format(case_id, result))
+        print("Case #{}:\n{}".format(case_id, result.strip()))
 
 
 if __name__ == '__main__':
